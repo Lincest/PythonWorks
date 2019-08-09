c1495a1337d6900f6285aedc876706eecd30bf82
Lincest <474617032@qq.com>
2019-08-09 16:16:39 +0800

    refresh

--- math_model/Malthusianism/logistic.py
+++ math_model/Malthusianism/logistic.py
@@ -10,22 +10,34 @@ K:                 最多人口数（常量）
 '''
 
 t=0
-Nt = 5.5 # 初始状态人口数
+Nt = 0 # 初始状态人口数
 N0=Nt
-tx=[]
-Nty=[]
+Nt_1=[]
+Nt_0=[N0]
 K = 20
 r = 1.9
-for i in range(0,50):
-    t+=1
-    tx.append(t)
-    Nt=(1+r)*Nt-(r/K)*(pow(Nt,2))
-    Nty.append(Nt)
+cnt = 0
+for i in range(0,10000):
+    Nt_0.append(Nt)
+    Nt += 0.005
+    Nt1=(1+r)*Nt-(r/K)*(pow(Nt,2))
+    t+=0.1
+    if(Nt1>0):
+        Nt_1.append(Nt1)
+        cnt=i
+    if(Nt1<0):
+        cnt=i
+        break
 
-plt.plot(tx,Nty,color="red")
+
+Nt_1 = Nt_1[:cnt]
+Nt_0 = Nt_0[:cnt]
+# plt.plot(Nt,Nt_0,color="red")
+plt.plot(Nt_0,Nt_1,color='green')
+plt.scatter(20,20,c='red') # plt.scatter(x, y, s=20, c='b') s是大小，标出K点所在位置
 plt.title("based with r="+str(r)+" and K="+str(K)+" and N0="+str(N0))
-plt.xlabel("t",color="blue")
-plt.ylabel("Nt",color="blue")
+plt.xlabel("Nt",color="blue")
+plt.ylabel("Nt+1",color="blue")
 plt.show()
 
 
